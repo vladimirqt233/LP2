b62d0ac68d9f7bfaf5075367f3e3fcf81411b8c1
RennzoxD <https://github.com/RennzoxD/DAD-2024-G1.git>
2024-08-26 18:39:45 -0500

    first commit

---
+++ SysAsistencia/src/main/java/pe/edu/upeu/sysasistencia/servicio/PeriodoService.java
@@ -0,0 +1,15 @@
+package pe.edu.upeu.sysasistencia.servicio;
+
+import pe.edu.upeu.sysasistencia.modelo.Periodo;
+
+import java.util.List;
+
+public interface PeriodoService {
+    public List<Periodo> obtenerPeriodos();
+    public Periodo obtenerPeriodo(Long id);
+    public void guardarPeriodo(Periodo periodo);
+    public void eliminarPeriodo(Long id);
+    public void actualizarPeriodo(Periodo periodo, Long id);
+
+
+}
